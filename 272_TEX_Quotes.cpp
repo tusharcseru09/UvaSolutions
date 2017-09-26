f7c44ff67a03da34278a4a218e3483d8555b8695
tusharcseru09 <tushar.cseru09@gmail.com>
2017-09-26 16:11:17 +0600

    TEX Quotes Left to Right fllip the quotes

---
+++ 272_TEX_Quotes.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    
+   string frase;
+   string str1="``"; 
+   string str2="''";
+   bool primero=true; 
+
+    while(getline (cin,frase)) {      
+      for(int i=0;i<frase.size();i++){
+          if(frase[i]=='"'){
+              if(primero){
+                  frase.erase(i,1);
+                  frase.insert(i,str1);       
+                  primero=false;        
+              }else{
+                  frase.erase(i,1);
+                  frase.insert(i,str2);
+                  primero=true; 
+              }  
+          }      
+      }   
+      cout<<frase<<endl;
+   }  
+ return 0;   
+}
