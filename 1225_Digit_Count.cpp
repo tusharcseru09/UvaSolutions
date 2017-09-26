b8251f47079aa04792a7d504068bb9144128c86c
tushar.cseru09 <tushar.cseru09@gmail.com>
2017-09-26 23:39:05 +0600

    In First Time Got Presentation Error! Carefull at the time of p rinting output, Space not allowed after last frequency print

--- 1225_Digit_Count.cpp
+++ 1225_Digit_Count.cpp
@@ -4,26 +4,26 @@
 int digitFrequency[10];
 
 int main(){
- 
+
 
  int n;
  scanf("%d",&n);
- 
+
  int inVal[n];
  for (int i=0; i<n; i++){
   scanf("%d",&inVal[i]);
  }
- 
+
  for (int i=0; i<n; i++){
-  
+
   int limit = inVal[i];
   int counter = limit;
-  
+
   for(int i=0; i<10; i++)
    digitFrequency[i]=0;
-  
+
   while (limit>0){
-   
+
    while(counter){
     digitFrequency[counter%10] ++;
     counter = counter/10;
@@ -33,11 +33,12 @@ int main(){
   }
   for(int i=0; i<9; i++)
    printf("%d ", digitFrequency[i]);
-   
+
   printf("%d\n",digitFrequency[9]);
  }
- 
+
 
  return 0;
 
 }
+
